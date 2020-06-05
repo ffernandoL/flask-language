d0f952e6a5a8eab7fae8bfd4019a0656383f01ee
Fernando Luis <fernnand.luis@gmail.com>
2020-06-04 22:03:38 -0300

    Adicionado a função deletar

--- app.py
+++ app.py
@@ -5,12 +5,27 @@ languages = ['Python']
 
 @app.route('/')
 def index():
-    return render_template('index.html', languages=languages)
+    return render_template('index.html', 
+        languages=languages)
+
+@app.route('/lang/<int:id>')
+def languag(id):
+    lang = languages[id]
+    return render_template(
+        'index.html',
+        languag=lang,
+        id=id
+    )
 
 @app.route('/add', methods=['POST'])
 def add():
     language = request.form['language']
     languages.append(language)
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    del languages[id]
     return redirect('/')
 
 if __name__ == '__main__':
